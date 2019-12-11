cf5444f64b95c6004d254d60016b9b66914e94bd
BiggsBottor <jonatan.lledo@gmail.com>
2019-12-11 13:34:51 +0100

    F-3 Menu bar *in progress*

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// import { RouterModule }   from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
+import {MenuModule} from 'primeng/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // RouterModule.forRoot([]),
+    MenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
