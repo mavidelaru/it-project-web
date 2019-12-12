a5f2079c89814af3c31dd19e40fd08ec4d6fa3e6
BiggsBottor <jonatan.lledo@gmail.com>
2019-12-12 10:20:43 +0100

    F-3 to dev Fresh restart

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('IT-Project-Web app is running!');
+    expect(page.getTitleText()).toEqual('itdashboard app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-// import { RouterModule }   from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-
-import {MenuModule} from 'primeng/menu';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    // RouterModule.forRoot([]),
-    MenuModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
