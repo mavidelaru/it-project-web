86880feb1272f9347e7df19b47dfcdf68a8ea13c
Diego Fernandez <diego.fernandez.perez@outlook.es>
2019-12-15 22:20:12 +0100

    F-4 Graficas

---
+++ src/app/components/stadistics/peoplepie/peoplepie.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PeoplepieComponent } from './peoplepie.component';
+
+describe('PeoplepieComponent', () => {
+  let component: PeoplepieComponent;
+  let fixture: ComponentFixture<PeoplepieComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PeoplepieComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PeoplepieComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/studentsMock.ts
@@ -0,0 +1,241 @@
+import { Student } from '../app/student';
+
+export const STUDENTS: Student[] = [
+    {
+        name: 'PANTINI, FABRIZIO',
+        absence: 5
+    },
+    {
+        name: 'KONTAXIS, PANAGIOTIS',
+        absence: 0
+    },
+    {
+        name: 'SANCHEZ ABAD, JAVIER',
+        absence: 4
+    },
+    {
+        name: 'FERNANDEZ NAVARRO, CRISTINA',
+        absence: 6
+    },
+    {
+        name: 'LEON HIDALGO, ESPERANZA',
+        absence: 2
+    },
+    {
+        name: 'CALVO RAMIREZ, JAVIER',
+        absence: 2
+    },
+    {
+        name: 'URIZAR TORRICO, BARLAN TITO',
+        absence: 0
+    },
+    {
+        name: 'ARIAS PIFERRER, JUDIT',
+        absence: 0
+    },
+    {
+        name: 'BOLEA SANCHO, DAVID',
+        absence: 3
+    },
+    {
+        name: 'PUNTI SABATES, CRISTIAN',
+        absence: 0
+    },
+    {
+        name: 'VIADER SAURET, GILBERT',
+        absence: 11
+    },
+    {
+        name: 'BARBARA ROVIRA, ZOE',
+        absence: 0
+    },
+    {
+        name: 'MARCH VERA, JORDI',
+        absence: 2
+    },
+    {
+        name: 'SABATE FABREGAT, JAUME',
+        absence: 1
+    },
+    {
+        name: 'AKSIMENKO, EVGENI',
+        absence: 1
+    },
+    {
+        name: 'TIMILSINA, BINAY',
+        absence: 5
+    },
+    {
+        name: 'VIDAL PANIAGUA, ANDREA',
+        absence: 11
+    },
+    {
+        name: 'CORTES NIEVES, PEDRO',
+        absence: 10
+    },
+    {
+        name: 'MAÑÁ ALVAREZ, DAVID',
+        absence: 0
+    },
+    {
+        name: 'MATALLANES BECERRA, MIQUEL',
+        absence: 7
+    },
+    {
+        name: 'FABREGAS SALINAS, CRISTIAN',
+        absence: 10
+    },
+    {
+        name: 'HASANBASIC GAZIVODA, VEDRANA',
+        absence: 4
+    },
+    {
+        name: 'MINGUELLA BLASCO, DANIEL',
+        absence: ''
+    },
+    {
+        name: 'LESCANO CORREA, MAGALI',
+        absence: 4
+    },
+    {
+        name: 'FERNANDEZ SUNYER, MAYA',
+        absence: 4
+    },
+    {
+        name: 'SAURA GARCIA, LUIS ALFONSO',
+        absence: 12
+    },
+    {
+        name: 'CASTRO SASTRE, RODRIGO',
+        absence: 0
+    },
+    {
+        name: 'SCATTOLINI, DARIO NICOLAS',
+        absence: 0
+    },
+    {
+        name: 'BELLINI, DANIEL',
+        absence: 11
+    },
+    {
+        name: 'CANALS REIG, JAUME',
+        absence: 6
+    },
+    {
+        name: 'CEVAS MUÑOZ, RODRIGO IGNACIO',
+        absence: 5
+    },
+    {
+        name: 'PASCUAL AGROMAYOR, ANA',
+        absence: 3
+    },
+    {
+        name: 'MARGALEF RIESTRA, XAVIER',
+        absence: 6
+    },
+    {
+        name: 'GONZALEZ VERGES, JORDI',
+        absence: 1
+    },
+    {
+        name: 'VILLEGAS PEÑA, CRISTIAN DANIEL',
+        absence: 2
+    },
+    {
+        name: 'BONET VALLVERDU, JORDI',
+        absence: 2
+    },
+    {
+        name: 'ZAJARA CAMILO, ALVARO',
+        absence: 5
+    },
+    {
+        name: 'BENEJAM MINGORANCE, SERGI',
+        absence: 0
+    },
+    {
+        name: 'GULYAEVA, ANASTASIA',
+        absence: 0
+    },
+    {
+        name: 'DEL MORAL ASENSIO, JAVIER',
+        absence: 3
+    },
+    {
+        name: 'CASTRO, ABRAHAM',
+        absence: 3
+    },
+    {
+        name: 'PRADO ROMERO, ARTURO ELMER',
+        absence: 6
+    },
+    {
+        name: 'LOPEZ MUÑOZ, PEDRO ALEJANDRO',
+        absence: 0
+    },
+    {
+        name: 'FERNANDEZ ARAMBURU LUMBIARRES, BEGOÑA',
+        absence: 8
+    },
+    {
+        name: 'FERNANDEZ FIAÑO, DANIEL',
+        absence: 8
+    },
+    {
+        name: 'CAMPO LEMUS, ANA VIRGINIA',
+        absence: 2
+    },
+    {
+        name: 'SCHVARZMAN MUÑOZ, PABLO',
+        absence: 6
+    },
+    {
+        name: 'LLEDO LLACER, JONATAN',
+        absence: 2
+    },
+    {
+        name: 'CID RODRIGUEZ, CRISTINA',
+        absence: 5
+    },
+    {
+        name: 'ASENSIO GARCIA, MARIO',
+        absence: 1
+    },
+    {
+        name: 'PEREIRA, EVANDRO',
+        absence: 3
+    },
+    {
+        name: 'ELIAS LAIRET, ALI TIUNA',
+        absence: 9
+    },
+    {
+        name: 'MAGDALENA CATALA, RAUL',
+        absence: 6
+    },
+    {
+        name: 'OGNIEVA, OLGA',
+        absence: 2
+    },
+    {
+        name: 'SANCHEZ FLORIDO, MIGUEL ANGEL',
+        absence: 5
+    },
+    {
+        name: 'NICODEMUS DE TORRES, ANGEL',
+        absence: 4
+    },
+    {
+        name: 'PAZOS ANANIA, EMILIANO',
+        absence: 5
+    },
+    {
+        name: 'MARTINEZ ESCOLAR, XAVIER',
+        absence: 1
+    }
+
+
+
+
+
+];
