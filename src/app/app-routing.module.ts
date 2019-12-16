b30f53a47c0667c3cc45569d95523c98442ba25b
Marta <mavidelaru@gmail.com>
2019-12-16 12:46:50 +0100

    Merge conflicts resolved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,10 @@ import { ClassroomComponent } from './components/classroom/classroom.component';
 import { RevisionComponent } from './components/revision/revision.component';
 import { StudentComponent } from './components/student/student.component';
 
-<<<<<<< HEAD
-=======
 import { ResetPasswordRequestComponent } from './components/reset-password-request/reset-password-request.component';
 import { LoginComponent } from './components/login/login.component';
 import { CommonModule } from '@angular/common';
 
->>>>>>> 688af1aec84dcc099727f234d8b955c37d6ada94
 
 const routes: Routes = [
   {
@@ -26,20 +23,17 @@ const routes: Routes = [
   },
   {
     path: 'student', component: StudentComponent
-<<<<<<< HEAD
-=======
   },
   {
     path: 'reset-password', component: ResetPasswordRequestComponent
   },
   {
     path: 'login', component: LoginComponent
->>>>>>> 688af1aec84dcc099727f234d8b955c37d6ada94
   }
 ];
 
 @NgModule({
-  imports: [ 
+  imports: [
     CommonModule,
     RouterModule.forRoot(routes)
   ],
--- src/app/components/stadistics/stadistics.component.ts
+++ src/app/components/stadistics/stadistics.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-
-=======
 import { Student } from '../../../app/student';
 import { STUDENTS } from '../../../app/studentsMock';
->>>>>>> 688af1aec84dcc099727f234d8b955c37d6ada94
 @Component({
   selector: 'app-stadistics',
   templateUrl: './stadistics.component.html',
@@ -12,13 +8,10 @@ import { STUDENTS } from '../../../app/studentsMock';
 })
 export class StadisticsComponent implements OnInit {
 
-<<<<<<< HEAD
-=======
   students = STUDENTS;
   selectedStudent: Student;
 
 
->>>>>>> 688af1aec84dcc099727f234d8b955c37d6ada94
   constructor() { }
 
   ngOnInit() {
