cd156311db35894fa196017b8128e22119157d30
Diego Fernandez <diego.fernandez.perez@outlook.es>
2019-12-19 18:26:22 +0100

    F-8 Test_modal

--- src/app/components/classroom/classroom.component.ts
+++ src/app/components/classroom/classroom.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { Alumne } from 'src/app/alumne';
 
 @Component({
   selector: 'app-classroom',
   templateUrl: './classroom.component.html',
   styleUrls: ['./classroom.component.scss']
 })
-export class ClassroomComponent implements OnInit {
+export class ClassroomComponent {
 
-  constructor() { }
+  public alumnes: Alumne[] = [
+    { student: 'pepe', itinerary: 'Front' },
+    { student: 'maria', itinerary: 'Back' },
+  ];
 
-  ngOnInit() {
+  selectedStudent: Alumne;
+
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) { }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
+  selectStudent(student: Alumne) {
+    this.selectedStudent = student;
+  }
 }
